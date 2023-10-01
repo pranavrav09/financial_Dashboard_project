bf162a0d3d5d5a872d9a9f69d13cead13711e097
TanayKapoor <68135212+TanayKapoor69@users.noreply.github.com>
2023-10-01 14:10:55 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import dash
 from dash import dcc, html, Input, Output
 import dash_bootstrap_components as dbc
-
+import yfinance as yf
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+import scipy.stats as stats
+from datetime import datetime, timedelta
+from sklearn.preprocessing import scale
+from sklearn.model_selection import train_test_split
+from matplotlib.ticker import FuncFormatter
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = html.Div([
@@ -9,8 +18,8 @@ app.layout = html.Div([
     html.Button("Univariate Analysis", id="univariate-button"),
     html.Button("Multivariate Analysis", id="multivariate-button"),
     html.Div(id="ticker-input", children=[]),
+    html.Div(id="output-div", children=[]),
 ])
-
 
 @app.callback(
     Output('ticker-input', 'children'),
@@ -18,10 +27,33 @@ app.layout = html.Div([
 )
 def get_ticker_input(n_clicks):
     if n_clicks:
-        return dbc.Input(id="ticker-input-box", placeholder="Enter Ticker Symbol", type="text", value="")
+        TICKER =  dbc.Input(id="ticker-input-box", placeholder="Enter Ticker Symbol", type = "text", value ="")
+        START_DATE =dbc.Input(id = "start-date-input-box", placeholder = "Enter Start data", type = "text", value = "")
+        END_DATE =  dbc.Input(id="end-date-input-box", placeholder="Enter End data", type="text", value="")
+
+
+        return [TICKER, START_DATE, END_DATE]
     else:
         return []
 
 
+@app.callback(
+    Output('output-div', 'children'),
+    Input('ticker-input-box', 'value'),
+    Input('start-date-input-box', 'value'),
+    Input('end-date-input-box', 'value')
+)
+def update_output(ticker, start_date, end_date, stockPrice):
+    return html.Div([
+        html.P(f"Ticker: {ticker}"),
+        html.P(f"Start Date: {start_date}"),
+        html.P(f"End Date: {end_date}"),
+        html.P(f"Stock Price: {stockPrice}")
+    ])
+
+def getStockPrice(TICKER, START_DATE, END_DATE):
+    stockPrice = yf.download(TICKER, START_DATE, END_DATE)
+
 if __name__ == '__main__':
     app.run_server(debug=True)
+
