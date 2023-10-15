b89a215c0f9e352c0f20d084039772dcf7a8c748
pranavrav09 <71946217+pranavrav09@users.noreply.github.com>
2023-10-15 11:31:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import dash
-from dash import dcc, html, Input, Output
+from dash import dcc, html, Input, Output, dash_table
 import dash_bootstrap_components as dbc
 import yfinance as yf
-import numpy as np
 import pandas as pd
+import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
 import scipy.stats as stats
@@ -11,6 +11,10 @@ from datetime import datetime, timedelta
 from sklearn.preprocessing import scale
 from sklearn.model_selection import train_test_split
 from matplotlib.ticker import FuncFormatter
+
+# Import Plotly
+import plotly.express as px
+
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = html.Div([
@@ -19,41 +23,59 @@ app.layout = html.Div([
     html.Button("Multivariate Analysis", id="multivariate-button"),
     html.Div(id="ticker-input", children=[]),
     html.Div(id="output-div", children=[]),
+    dcc.Graph(id="stock-histogram")  # Add a Graph component for the histogram
 ])
+
 
 @app.callback(
     Output('ticker-input', 'children'),
     Input('univariate-button', 'n_clicks')
 )
 def get_ticker_input(n_clicks):
-    if n_clicks:
-        TICKER =  dbc.Input(id="ticker-input-box", placeholder="Enter Ticker Symbol", type = "text", value ="")
-        START_DATE =dbc.Input(id = "start-date-input-box", placeholder = "Enter Start data", type = "text", value = "")
-        END_DATE =  dbc.Input(id="end-date-input-box", placeholder="Enter End data", type="text", value="")
-
-
+    if n_clicks and n_clicks > 0:
+        TICKER = dbc.Input(id="ticker-input-box", placeholder="Enter Ticker Symbol", type="text")
+        START_DATE = dcc.DatePickerSingle(id="start-date-input-box", placeholder="Enter Start Date")
+        END_DATE = dcc.DatePickerSingle(id="end-date-input-box", placeholder="Enter End Date")
         return [TICKER, START_DATE, END_DATE]
-    else:
-        return []
+    return []
 
 
 @app.callback(
     Output('output-div', 'children'),
+    Output('stock-histogram', 'figure'),  # Output for the stock histogram
     Input('ticker-input-box', 'value'),
-    Input('start-date-input-box', 'value'),
-    Input('end-date-input-box', 'value')
+    Input('start-date-input-box', 'date'),
+    Input('end-date-input-box', 'date')
 )
-def update_output(ticker, start_date, end_date, stockPrice):
-    return html.Div([
-        html.P(f"Ticker: {ticker}"),
-        html.P(f"Start Date: {start_date}"),
-        html.P(f"End Date: {end_date}"),
-        html.P(f"Stock Price: {stockPrice}")
-    ])
+def update_output(ticker, start_date, end_date):
+    if ticker and start_date and end_date:
+        df = get_stock_data(ticker, start_date, end_date)
+        if df is not None:
+            table = dash_table.DataTable(
+                id='table',
+                columns=[{"name": i, "id": i} for i in df.columns],
+                data=df.to_dict('records'),
+                style_table={'overflowX': 'scroll'},
+                page_size=10
+            )
 
-def getStockPrice(TICKER, START_DATE, END_DATE):
-    stockPrice = yf.download(TICKER, START_DATE, END_DATE)
+            # Create a histogram of stock values
+            fig = px.histogram(df, x="Adj Close", title=f"Histogram of {ticker} Stock Values")
+            return [table, fig]  # Return the table and the histogram
+    return [], {}
+
+
+def get_stock_data(ticker, start_date, end_date):
+    try:
+        stock_data = yf.download(ticker, start_date, end_date)
+        stock_data.reset_index(inplace=True)
+        stock_data['Year'] = stock_data['Date'].dt.year
+        yearly_data = stock_data.groupby('Year').mean()
+        return yearly_data
+    except Exception as e:
+        print(f"Error fetching data: {e}")
+        return None
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
